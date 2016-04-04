1dfe89bcca061f9344fc1cb2a57bc494e9e46102
Vitaliy Yarkun <AdamFox@macbook-pro-vitaliy.local>
2016-04-04 19:39:48 +0300

    Initial Commit

---
+++ BuchRechMC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BuchRechMC
+//
+//  Created by Vitaliy Yarkun on 4/4/16.
+//  Copyright © 2016 Vitaliy Yarkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BuchRechMC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuchRechMC
+//
+//  Created by Vitaliy Yarkun on 4/4/16.
+//  Copyright © 2016 Vitaliy Yarkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
