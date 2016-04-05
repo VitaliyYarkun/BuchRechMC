c26770f4aaf0b4fa39fea964189780a3e205ad89
Vitaliy Yarkun <AdamFox@mbp-vitaliy.dlink>
2016-04-05 22:45:40 +0300

    GL

---
+++ BuchRechMC/GLController.h
@@ -0,0 +1,15 @@
+//
+//  GLController.h
+//  BuchRechMC
+//
+//  Created by Vitaliy Yarkun on 4/5/16.
+//  Copyright © 2016 Vitaliy Yarkun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIWebView *glWebView;
+
+@end
